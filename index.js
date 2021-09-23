e57f8699a4efe25b3333067df51422438b009b11
Akash <akashshukla506@gmail.com>
2021-09-23 20:02:29 +0530

    Added category based api

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
+// MAIN BACKEND FILE
+
 const db = require("./database");
-console.log(db.books);
-console.log(db.authors);
-console.log(db.publications);
+// console.log(db.books);
+// console.log(db.authors);
+// console.log(db.publications);
 
 const express = require("express");
 
 const app = express();
 
+// http://localhost:3000/
 app.get("/", (req, res) => {
     const getAllBooks = db.books;
     return res.json(getAllBooks);
-})
+});
 
-app.get("/", (req, res) => {
-    const getAllBooks = db.books;
-    return res.json(getAllBooks);
-})
+// http://localhost:3000/book-isbn/12345Two
+app.get("/book-isbn/:isbn", (req, res) => {
+    // console.log(req.params);
+    const {isbn} = req.params;
+    // console.log(isbn);
+    const getSpecificBook = db.books.filter((book) => book.ISBN === isbn);
+    // console.log(getSpecificBook);
+    // console.log(getSpecificBook.length);
+    if(getSpecificBook.length===0) {
+        return res.json({"error": `No Book found for the ISBN of ${isbn}`});
+    }
+    return res.json(getSpecificBook[0]);
+});
+
+// http://localhost:3000/book-category/programming
+app.get("/book-category/:category", (req, res) => {
+    // // console.log(req.params);
+    const {category} = req.params;
+    // // console.log(isbn);
+    const getSpecificBooks = db.books.filter((book) => book.category.includes(category));
+    // // console.log(getSpecificBook);
+    // // console.log(getSpecificBook.length);
+    if(getSpecificBooks.length===0) {
+        return res.json({"error": `No Books found for the category of ${category}`});
+    }
+    return res.json(getSpecificBooks);
+});
+
 
 app.listen(2000, () => {
-    console.log("My Express app is running");
+    console.log("MY EXPRESS APP IS RUNNING.....")
 });
