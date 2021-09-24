fe5a5995fc7d228854bfcf4bd3155f4367b5170d
Akash <akashshukla506@gmail.com>
2021-09-24 20:32:33 +0530

    Added Put and Delete APIs

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ const express = require("express");
 
 const app = express();
 
-// http://localhost:3000/
+app.use(express.json());
+
+// http://localhost:2000/
 app.get("/", (req, res) => {
+    return res.json({"WELCOME": `to my Backend Software for the Book Company`});
+});
+
+// http://localhost:2000/books
+app.get("/books", (req, res) => {
     const getAllBooks = db.books;
     return res.json(getAllBooks);
 });
 
-// http://localhost:3000/book-isbn/12345Two
+// http://localhost:2000/book-isbn/12345Two
 app.get("/book-isbn/:isbn", (req, res) => {
     // console.log(req.params);
     const {isbn} = req.params;
@@ -29,7 +36,7 @@ app.get("/book-isbn/:isbn", (req, res) => {
     return res.json(getSpecificBook[0]);
 });
 
-// http://localhost:3000/book-category/programming
+// http://localhost:2000/book-category/programming
 app.get("/book-category/:category", (req, res) => {
     // // console.log(req.params);
     const {category} = req.params;
@@ -43,6 +50,143 @@ app.get("/book-category/:category", (req, res) => {
     return res.json(getSpecificBooks);
 });
 
+// http://localhost:2000/authors
+app.get("/authors", (req, res) => {
+    const getAllAuthors = db.authors;
+    return res.json(getAllAuthors);
+});
+
+// http://localhost:2000/author/1
+app.get("/author/:id", (req, res) => {
+    // console.log(req.params);
+    let {id} = req.params;
+    id = Number(id);
+    // console.log(id);
+    const getSpecificAuthor = db.authors.filter((author) => author.id === id);
+    // console.log(getSpecificAuthor);
+    // console.log(getSpecificAuthor.length);
+    if(getSpecificAuthor.length===0) {
+        return res.json({"error": `No Author found for the id of ${id}`});
+    }
+    return res.json(getSpecificAuthor[0]);
+});
+
+// http://localhost:2000/author/12345Two
+app.get("/author/:isbn", (req, res) => {
+   
+});
+
+// http://localhost:2000/publications
+app.get("/publications", (req, res) => {
+    const getAllPublications = db.publications;
+    return res.json(getAllPublications);
+});
+
+// http://localhost:2000/publications-isbn/12345Two
+// TOBEFIXED
+app.get("/publications-isbn/:isbn", (req, res) => {
+    // console.log(req.params);
+    let {isbn} = req.params;
+    // console.log(isbn);
+    const getSpecificPublication = db.publications.filter((publication) => publication.ISBN === isbn);
+    // console.log(getSpecificBook);
+    // console.log(getSpecificBook.length);
+    if(getSpecificPublication.length===0) {
+        return res.json({"error": `No Publication found for the ISBN of ${isbn}`});
+    }
+    return res.json(getSpecificPublication[0]);
+});
+
+
+
+//http://localhost:2000/book
+app.post("/book", (req,res) => {
+    //console.log(req.body);
+    db.books.push(req.body);
+    return res.json(db.books);
+});
+
+//http://localhost:2000/author
+app.post("/author", (req,res) => {
+    //console.log(req.body);
+    db.authors.push(req.body);
+    return res.json(db.authors);
+});
+
+// http://localhost:2000/book-update/12345ONE
+app.put("/book-update/:isbn", (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    const {isbn} = req.params;
+    db.books.forEach((book) => {
+        if(book.ISBN === isbn) {
+            console.log({...book, ...req.body})
+            return {...book, ...req.body};
+        }
+        return book;
+    })
+    return res.json(db.books);
+});
+
+// http://localhost:2000/author-update/1
+app.put("/author-update/:id", (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    const {id} = req.params;
+    db.authors.forEach((author) => {
+        if(author.ID === id) {
+            console.log({...author, ...req.body})
+            return {...author, ...req.body};
+        }
+        return author;
+    })
+    return res.json(db.authors);
+});
+
+// http://localhost:2000/publication-update/1
+// To be Done
+app.put("/author-update/:id", (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    const {id} = req.params;
+    db.authors.forEach((author) => {
+        if(author.ID === id) {
+            console.log({...author, ...req.body})
+            return {...author, ...req.body};
+        }
+        return author;
+    })
+    return res.json(db.authors);
+});
+
+
+// http://localhost:2000/book-delete/12345ONE
+app.delete("/book-delete/:isbn", (req, res) => {
+    console.log(req.params);
+    const {isbn} = req.params;
+    const filteredBooks = db.books.filter((book) =>  book.ISBN!==isbn); 
+    console.log(filteredBooks);
+    db.books = filteredBooks;
+    return res.json(db.books);
+});
+
+// http://localhost:2000/book-author-delete/12345ONE/1
+app.delete("/book-author-delete/:isbn/:id", (req, res) => {
+    console.log(req.params);
+    let {isbn, id} = req.params;
+    id = Number(id);
+    db.books.forEach((book) =>  {
+        if(book.ISBN === isbn) {
+            if(!book.authors.includes(id)) {
+                return;
+            }
+            book.authors = book.authors.filter((author) => author!==id);
+            return book;
+        }
+        return book;
+    }); 
+    return res.json(db.books);
+});
 
 app.listen(2000, () => {
     console.log("MY EXPRESS APP IS RUNNING.....")
